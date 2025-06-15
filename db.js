b719b159601ac8a1a046a00adb591032e1fa9305
Sharuk22 <sharukrukunayaka@gmail.com>
2025-06-15 22:40:51 +0530

    Update code

--- db.js
+++ db.js
@@ -17,12 +17,23 @@
 import pkg from 'pg';   // 'pg' package එක import කර ගන්න
 const { Pool } = pkg;   // destructure Pool class එක
 
+const { Pool } = require('pg');
+
 const pool = new Pool({
-  host: "pgm-zf88260fyr2nijpmvo.rwlb.kualalumpur.rds.aliyuncs.com",
-  user: "cerpuser",
-  port: 5432,
-  password: "Ati@987K",
-  database: "cerp"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
+
+// const pool = new Pool({
+//   host: "pgm-zf88260fyr2nijpmvo.rwlb.kualalumpur.rds.aliyuncs.com",
+//   user: "cerpuser",
+//   port: 5432,
+//   password: "Ati@987K",
+//   database: "cerp"
+// });
 
 export default pool;
